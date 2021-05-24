2942e04444584d6da5e218efe69675ca722ad34f
Avadhut Patade <avadhutpatade1@gmail.com>
2021-05-24 15:30:52 +0530

    Add dependancy, bean and properties for email

--- src/main/java/com/example/configuration/BeansConfigurer.java
+++ src/main/java/com/example/configuration/BeansConfigurer.java
@@ -4,18 +4,34 @@ import com.example.Application;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.Properties;
 
 @Configuration
 public class BeansConfigurer {
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private Integer port;
+
+    @Value("${spring.mail.sender.email}")
+    private String username;
+
+    @Value("${spring.mail.sender.password}")
+    private String password;
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
@@ -29,6 +45,21 @@ public class BeansConfigurer {
         headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
         HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
         return entity;
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost(host);
+        javaMailSender.setPort(port);
+        javaMailSender.setUsername(username);
+        javaMailSender.setPassword(password);
+        Properties props = javaMailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        return javaMailSender;
     }
 
     @Bean
